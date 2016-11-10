98dfffd9a1f22462f5f29157e4fc096b9ff35038
Alexander Houston <ahouston@liquidweb.com>
2016-11-10 17:44:53 -0500

    added in !help command. still needs work to make it sound more natural

--- commands/help/index.js
+++ commands/help/index.js
@@ -1,5 +1,14 @@
 var slack = require('../../lib/slack');
 
+var helpMsg =
+`commands:
+*!brb* [min] - take break for [min] minutes (from auto-assign queue)
+	* does not automatically log you back in, but you'll get pinged
+*!back* - log back into the queue
+*!out* [user] - log out of the queue for an indefinite period of time
+	* if given, logs out [user]
+*!help* - show this help message`
+
 module.exports = {
 	expr: /^!help/,
 	run: function (data) {
@@ -8,5 +17,5 @@ module.exports = {
 };
 
 function brb(data) {
-	slack.sendMessage('help function not yet ready', data.channel);
+	slack.sendMessage(helpMsg, data.channel);
 }
