42de7edd945c2ca70835e1e9340e09a76e35d6eb
Alexander Houston <ahouston@liquidweb.com>
2016-11-10 17:44:20 -0500

    fleshed out the !brb command a little. nothing too serious, just parsing the input

--- commands/brb/index.js
+++ commands/brb/index.js
@@ -8,6 +8,10 @@ module.exports = {
 };
 
 function brb(data) {
-	slack.sendMessage('brb function not yet ready', data.channel);
+	var arg = data.text.split(" ")[1];
+	//default break time is 5 minutes
+	var breakTime = parseInt(arg) ? parseInt(arg).toString() : "5";
+
+	slack.sendMessage("Set break for " + slack.dataStore.getUserById(data.user).name + " for " + breakTime + " minutes.", data.channel);
 }
 
