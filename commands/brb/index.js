db4e1578af532c315de5d7f4b4ead7964a3358e1
Alexander Houston <ahouston@liquidweb.com>
2016-11-15 18:19:49 -0500

    started fleshing out !brb command. currently sets users to "not accepting chats" and sets the timer. todo: implement reminders to log back in

--- commands/brb/index.js
+++ commands/brb/index.js
@@ -1,4 +1,10 @@
 var slack = require('../../lib/slack');
+var requests = require('./lc_requests.js');
+
+var lc_user = require('../../config').lcAPIUser;
+var lc_key = require('../../config').lcAPIKey;
+
+var overbreak = [];
 
 module.exports = {
 	expr: /^!brb/,
@@ -8,10 +14,28 @@ module.exports = {
 };
 
 function brb(data) {
-	var arg = data.text.split(" ")[1];
+	var arg;
+    var breakTime;
+
+    arg = data.text.split(" ")[1];
 	//default break time is 5 minutes
-	var breakTime = parseInt(arg) ? parseInt(arg).toString() : "5";
+    breakTime = parseInt(arg) ? parseInt(arg).toString() : "5";
 
-	slack.sendMessage("Set break for " + slack.dataStore.getUserById(data.user).name + " for " + breakTime + " minutes.", data.channel);
+    //checks agent state
+    requests.getState(slack.dataStore.getUserById(data.user).name);
+
+    //sets state to "not accepting chats"
+    requests.changeState(slack.dataStore.getUserById(data.user).name, "not accepting chats", function() {
+    	setTimeout(function breakUp() {
+
+            slack.sendMessage(slack.dataStore.getUserById(data.user).name + ": your break is up. Please use *!back* to log back into chats", data.channel);
+
+            //TODO
+			//setInterval("reminder")
+
+    	}, breakTime * 60 * 1000);
+
+        slack.sendMessage("Set break for " + slack.dataStore.getUserById(data.user).name + " for " + breakTime + " minutes.", data.channel);
+	});
+
 }
-
