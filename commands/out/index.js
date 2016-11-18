bb6fe06042db5cd57935b1166eb49d94c0405b5c
Alexander Houston <ahouston@liquidweb.com>
2016-11-17 19:11:04 -0500

    expanded on the !out command. users can use it to log themselves out for an indefinite period of time, or they log other users out (e.g. !brb ahouston).
    also technically silences reminders for a user

--- commands/out/index.js
+++ commands/out/index.js
@@ -1,12 +1,53 @@
 var slack = require('../../lib/slack');
+var requests = require('../lc_requests');
+var breaks = require('../breaks');
 
 module.exports = {
 	expr: /^!out/,
 	run: function (data) {
-		brb(data);
+		out(data);
 	}
 };
 
-function brb(data) {
-	slack.sendMessage('out function not yet ready', data.channel);
+function out(data) {
+
+    var username = slack.dataStore.getUserById(data.user).name;
+    var arg = data.text.split(' ')[1];
+
+    //log out user
+	if (arg == undefined) {
+		logOut(
+			username,
+			//callback
+			function (response) {
+            slack.sendMessage(username +
+                ': you have been logged out of chats. Please use *!back* to log back in when you are ready',
+                data.channel);
+            breaks.clearBreaks(username);
+		});
+
+	} else {
+		//log out user
+		logOut(
+			arg,
+			//callback
+			function(response) {
+            	slack.sendMessage(
+            		'Logged out ' + arg +
+					'. Please use *!back* to log back in when you are ready',
+                	data.channel);
+				breaks.clearBreaks(username);
+			});
+	}
 }
+
+function logOut(user, callback) {
+    requests.changeStatus(
+        user,
+        "not accepting chats",
+		//callback
+        function (response) {
+            callback(response);
+        });
+}
+
