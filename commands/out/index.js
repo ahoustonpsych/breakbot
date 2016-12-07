2caa507cf425f695824ca064d2bc657a0b4f14ac
Alexander Houston <ahouston@liquidweb.com>
2016-12-07 10:33:50 -0500

    implemented user validation for the !out command. if [user] is supplied to !out [user], then [user] is passed through getUserByName() to determine if it exists.
    the command will also try to obtain the slack user's email to ensure the right user gets logged out.
    
    finally, added some logging so we can see who uses !out

--- commands/out/index.js
+++ commands/out/index.js
@@ -18,19 +18,35 @@ module.exports = {
 
 function out(data) {
 
-    var user = slack.dataStore.getUserById(data.user).name;
+    var username = undefined;
+    var user = slack.dataStore.getUserById(data.user).profile.email.split('@')[0];
     var arg = data.text.split(' ')[1];
 
-    //logs out [user] if given
-	//otherwise, logs out user that used !out
-    var username = arg != undefined ? arg : user; // TODO: validate this info
+    if (arg) {
+        try {
+            username = slack.dataStore.getUserByName(arg).profile.email.split('@')[0];
+        }
+        catch (e) {
+            slack.sendMessage("Invalid user: " + arg, data.channel);
 
+            //logging
+            console.error(new Date() + ': ' + user + ' used !out with invalid user "' + arg + '"');
+            return;
+        }
+    }
+    else {
+        username = user;
+    }
     logOut(username,
-		function(response) {
+        function (response) {
             slack.sendMessage(
                 'Logged out ' + username +
                 '. Please use *!back* to log back in when you are ready',
                 data.channel);
+
+            //logging
+            console.log(new Date() + ': logged out ' + username + ' with !out');
+
             breaks.clearBreaks(username);
             breaks.out[username] = 1;
         });
@@ -45,4 +61,3 @@ function logOut(username, callback) {
             callback(response);
         });
 }
-
