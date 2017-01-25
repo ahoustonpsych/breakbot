0fbe0ae099b87ffa6271d571db917b71f3a8bc1a
Alexander Houston <ahouston@liquidweb.com>
2017-01-25 18:53:06 -0500

    `!restart` now saves break data to a file if used

--- commands/restart/index.js
+++ commands/restart/index.js
@@ -1,30 +1,43 @@
 var slack = require('../../lib/slack').rtm;
 var db = require('../../lib/database');
 
+var breaks = require('../breaks');
+
 module.exports = {
     expr: /^(!restart)|(breakbot:? restart)/i,
-    run: restart
+    run: restart,
 };
 
 function restart(data) {
 
-    var logdata = {
-        username: slack.dataStore.getUserById(data.user).name,
-        command: '!restart',
-        date: 'now'
-    };
-
-    slack.sendMessage('Restarting...', data.channel);
-
-    /* logging */
-    db.log('command_history', logdata)
+    breaks.saveBreaks()
         .then(function () {
-            /* self-destruct one ticket after logging */
-            process.nextTick(function () {
-                process.exit();
-            });
+
+            console.log('saved breaks successfully');
+
+            var logdata = {
+                username: slack.dataStore.getUserById(data.user).name,
+                command: '!restart',
+                date: 'now'
+            };
+
+            slack.sendMessage('Restarting...', data.channel);
+
+            /* logging */
+            db.log('command_history', logdata)
+                .then(function () {
+
+                    /* self-destruct one tick after logging */
+                    process.nextTick(function () {
+                        process.exit();
+                    });
+
+                })
+                .catch(function (err) {
+                    console.error('ERROR LOGGING COMMAND', err);
+                });
         })
         .catch(function (err) {
-            console.error('ERROR LOGGING COMMAND', err);
+            console.error(err);
         });
 }
