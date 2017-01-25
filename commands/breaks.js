5fb3c13a336a2d76ec25042f5f7ed61477ac633a
Alexander Houston <ahouston@liquidweb.com>
2017-01-25 18:54:57 -0500

    added data persistence so breaks won't get nuked if breakbot has to be restarted. currently it saves all breaks to a file defined in config.js, then reloads them on startup.

--- commands/breaks.js
+++ commands/breaks.js
@@ -1,3 +1,9 @@
+var fs = require('fs');
+
+var Promise = require('promise');
+
+var conf = require('../conf/config');
+
 var onbreak = {};
 var overbreak = {};
 var out = {};
@@ -10,14 +16,63 @@ module.exports = {
     out: out,
     lunch: lunch,
     bio: bio,
-    clearBreaks: function (user) {
-        clearBreaks(user);
-    }
+    clearBreaks: clearBreaks,
+    saveBreaks: saveBreaks,
+    restoreBreaks: restoreBreaks
 };
 
 function clearBreaks(user) {
-    delete onbreak[user];
-    delete overbreak[user];
-    delete lunch[user];
-    delete bio[user];
+    delete this.onbreak[user];
+    delete this.overbreak[user];
+    delete this.lunch[user];
+    delete this.bio[user];
+}
+
+function saveBreaks() {
+
+    var that = this;
+
+    return new Promise(function (fulfill, reject) {
+
+        /* all break data */
+        var breakdata = JSON.stringify(that.onbreak) + '\n' +
+            JSON.stringify(that.overbreak) + '\n' +
+            JSON.stringify(that.out) + '\n' +
+            JSON.stringify(that.lunch) + '\n' +
+            JSON.stringify(that.bio);
+
+        if (breakdata !== undefined) {
+            fs.writeFileSync(conf.savefile, breakdata);
+            fulfill('success');
+        }
+
+        else reject('empty break data somehow: ' + breakdata);
+
+    });
+}
+
+function restoreBreaks() {
+
+    var that = this;
+
+    fs.readFile(conf.savefile, 'utf8', function (err,res) {
+
+        if (err) console.error('not found');
+
+        else if (!res) {
+            console.log('no break data in file');
+        }
+
+        else {
+
+            var rawbreaks = res.split('\n');
+
+            that.onbreak = JSON.parse(rawbreaks[0]);
+            that.overbreak = JSON.parse(rawbreaks[1]);
+            that.out = JSON.parse(rawbreaks[2]);
+            that.lunch = JSON.parse(rawbreaks[3]);
+            that.bio = JSON.parse(rawbreaks[4]);
+
+        }
+    });
 }
