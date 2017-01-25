0ce8c2943658df10630dbf3371e2a80a94d9ac85
Alexander Houston <ahouston@liquidweb.com>
2017-01-25 10:08:02 -0500

    `!rm` can now remove names with capitals from the topic

--- commands/rm/index.js
+++ commands/rm/index.js
@@ -18,7 +18,7 @@ function rm(data) {
     else
         off = offs['breakbot'];
 
-    /* try to parse the arg if given. if it can't, defaults to the user that sent the message*/
+    /* try to parse the args if given. if it can't, defaults to the user that sent the message */
     var arg = data.text.split(' ')
         .slice(off)
         .map(function (el) {
@@ -38,12 +38,22 @@ function rm(data) {
     });
 }
 
+/* top: current topic
+ * arg: list of people to remove from the topic
+ */
 function replaceChatter(top, arg, callback) {
     var re = '';
+
     arg.forEach(function (el) {
-        re = new RegExp('(,|, | )' + el, 'g');
-        if (top.indexOf(el) !== null)
+
+        /* searches for a name in the topic and removes it if found */
+        if (top.match(new RegExp(el, 'gi')) !== null) {
+            re = new RegExp('(,|, | )' + el, 'gi');
             top = top.replace(re, '');
+        }
+
     });
+
     callback(top);
+
 }
