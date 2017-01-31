6278749773e9c2ede062cbfe5b7c032860fb9ae7
Alexander Houston <ahouston@liquidweb.com>
2017-01-31 14:08:23 -0500

    tweaked `!rm` to allow for upper-cased names in the topic

--- commands/rm/index.js
+++ commands/rm/index.js
@@ -18,32 +18,64 @@ function rm(data) {
     else
         off = offs['breakbot'];
 
-    /* try to parse the arg if given. if it can't, defaults to the user that sent the message*/
+    /* try to parse the args if given. if it can't, defaults to the user that sent the message */
     var arg = data.text.split(' ')
         .slice(off)
         .map(function (el) {
+
             el = topic.removeSpecial(el);
+
             if (el.match(/^me$/i) !== null || el === '')
                 return slack.dataStore.getUserById(data.user).name;
             else
                 return el;
+
         });
 
     if (arg instanceof Array)
         if (arg.length === 0)
             arg = [slack.dataStore.getUserById(data.user).name];
 
+    /* remove user(s) from topic */
     replaceChatter(newtopic, arg, function (top) {
-        topic.setTopic(data.channel, top);
+        //callback
+        if (!(top instanceof Array))
+            topic.setTopic(data.channel, top);
+
+        else
+            slack.sendMessage('not in topic: ' + top.join(' '), data.channel);
+
     });
 }
 
+/* top: current topic
+ * arg: list of people to remove from the topic
+ */
 function replaceChatter(top, arg, callback) {
+
     var re = '';
+    /* array of users not in topic */
+    var notintopic = [];
+
     arg.forEach(function (el) {
-        re = new RegExp('(,|, | )' + el, 'g');
-        if (top.indexOf(el) !== null)
+
+        /* searches for a name in the topic and removes it */
+        if (top.match(new RegExp(el, 'gi')) !== null) {
+            re = new RegExp('(,|, | )' + el, 'gi');
             top = top.replace(re, '');
+        }
+
+        /* take note if a user provided isn't in the topic */
+        else
+            notintopic.push(el);
+
     });
-    callback(top);
+
+    /* if none of the users provided are in the topic, then don't update it */
+    if (notintopic.length >= arg.length)
+        callback(notintopic);
+
+    else
+        callback(top);
+
 }
