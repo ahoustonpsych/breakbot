ddbebb8506f992b1552e0b8b4dc6ade52c0aee9f
Alexander Houston <ahouston@liquidweb.com>
2017-01-31 08:14:09 -0500

    lunch scheduler! currently supports:
    `!lunch 7:00` - set your lunch for 7:00
    `!lunch bnewman 8:00` - set bnewman's lunch for 8:00
    `!lunch list` - list all lunches
    `!lunch rm [user]` - remove [user] from the schedule
    
    it'll ping you 15 minutes before your schedule lunch, and when the time comes.
    
    finalish testing now

--- commands/lunch/index.js
+++ commands/lunch/index.js
@@ -4,11 +4,12 @@ var conf = require('../../conf/breaks.config');
 var db = require('../../lib/database');
 var requests = require('../lc_requests');
 var breaks = require('../breaks');
+var luncher = require('../luncher');
 
 var offs = {'!lunch': 1, 'breakbot': 2};
 
 /* lunch time */
-var time = 30;
+const _time = 30;
 
 module.exports = {
     expr: /^(!lunch)|(breakbot:? lunch)/i,
@@ -16,39 +17,50 @@ module.exports = {
 };
 
 function lunch(data) {
+
     if (data.text.split(' ')[0].match(/!lunch/i) !== null)
         off = offs['!lunch'];
     else
         off = offs['breakbot'];
 
     var username = slack.dataStore.getUserById(data.user).profile.email.split('@')[0];
+    var arg = data.text.split(' ')[off];
 
-    /* prevents users from logging out again if they're already logged out */
-    if (breaks.lunch[username] instanceof Object) {
-        slack.sendMessage('already on lunch', data.channel);
-        return;
+    if (arg) {
+        scheduler(data);
+        return true;
     }
 
-    delete breaks.out[username];
-    breaks.clearBreaks(username);
+    else {
 
-    /* sets agent status to "not accepting chats" */
-    slack.sendMessage('Set lunch for ' + username + '. See you in 30 minutes!', data.channel);
+        /* prevents users from logging out again if they're already logged out */
+        if (breaks.lunch[username] instanceof Object) {
+            slack.sendMessage('already on lunch', data.channel);
+            return;
+        }
 
-    setBreak(username, time, data.channel);
+        luncher.clearLunch(username);
+        delete breaks.out[username];
+        breaks.clearBreaks(username);
 
-    /* logging */
-    var logdata = {
-        username: username,
-        command: '!lunch',
-        duration: time,
-        date: 'now'
-    };
+        /* sets agent status to "not accepting chats" */
+        slack.sendMessage('Set lunch for ' + username + '. See you in 30 minutes!', data.channel);
 
-    db.log('command_history', logdata)
-        .catch(function (err) {
-            console.error('ERROR LOGGING COMMAND', err);
-        });
+        setBreak(username, _time, data.channel);
+
+        /* logging */
+        var logdata = {
+            username: username,
+            command: '!lunch',
+            duration: _time,
+            date: 'now'
+        };
+
+        db.log('command_history', logdata)
+            .catch(function (err) {
+                console.error('ERROR LOGGING COMMAND', err);
+            });
+    }
 }
 
 /*
@@ -68,4 +80,163 @@ function setBreak(username, time, channel) {
         .catch(function (err) {
             console.error('ERROR CHANGING STATUS', err);
         });
+}
+
+
+function scheduler(data) {
+
+    var user = slack.dataStore.getUserById(data.user).profile.email.split('@')[0];
+    var arg = data.text.split(' ')[off];
+
+    if (!arg)
+        return false;
+
+    //list command
+    if (arg.match(/list/i) !== null) {
+        list = luncher.listLunch();
+
+        if (!list) {
+            slack.sendMessage('nobody on lunch', data.channel);
+            return false;
+        }
+
+        else
+            slack.sendMessage(list, data.channel);
+
+        return true;
+    }
+
+    //rm command
+    else if (arg.match(/rm/i) !== null) {
+        name = data.text.split(' ')[off+1];
+
+        if (!name || name.match(/me/i) !== null)
+            name = user;
+
+        //fail if invalid user
+        if (!(slack.dataStore.getUserByName(name) instanceof Object)) {
+            slack.sendMessage('invalid user: ' + name, data.channel);
+            return false;
+        }
+
+        //fail if lunch time doesn't exist
+        else if (!(luncher.clearLunch(name))) {
+            slack.sendMessage('lunch not found for: ' + name, data.channel);
+            return false;
+        }
+
+        else {
+            slack.sendMessage('removed lunch for: ' + name, data.channel);
+            return true;
+        }
+
+    }
+
+    //match username if possible
+    if (slack.dataStore.getUserByName(arg) instanceof Object) {
+        user = slack.dataStore.getUserByName(arg).profile.email.split('@')[0];
+        time = data.text.split(' ')[off + 1];
+    }
+
+    //match 'me'
+    else if (arg.match(/me/i) !== null) {
+        time = data.text.split(' ')[off + 1];
+    }
+
+    //no arg given
+    else
+        time = arg;
+
+    if (!time) {
+        slack.sendMessage('no time given', data.channel);
+        return false;
+    }
+
+    lunch = parseTime(time);
+
+    //fail if time is invalid
+    if (!lunch) {
+        slack.sendMessage('invalid time: ' + time, data.channel);
+        return false;
+    }
+
+    //fail if slot already taken
+    if (!(luncher.checkDupe(lunch))) {
+        slack.sendMessage('slot already taken: ' + time, data.channel);
+        return false;
+    }
+
+    //add lunch if all else is good
+    else {
+        //but fail if they already have lunch scheduled
+        if (!(luncher.addLunch(user, lunch, data.channel)))
+            slack.sendMessage('already scheduled: ' + user, data.channel);
+        else {
+            //minute = (lunch.getMinutes().toString().length < 2) ? '0' + lunch.getMinutes() : lunch.getMinutes();
+            slack.sendMessage('Set lunch for: ' + user, data.channel);
+        }
+    }
+}
+
+//returns date obj if time is valid, false otherwise
+function parseTime(time) {
+
+    var hour = -1;
+    var minute = -1;
+
+    var timeobj = new Date();
+
+    var lunchtime = undefined;
+
+    if (typeof(time) !== 'string')
+        return false;
+
+    //12:00
+    if (time.match(/:/) !== null) {
+        hour = parseInt(time.split(':')[0]);
+        minute = parseInt(time.split(':')[1]);
+    }
+
+    //1200
+    else if (time.match(/[0-9]{4}/) !== null) {
+        hour = parseInt(time.slice(0, 2));
+        minute = parseInt(time.slice(2, 4));
+    }
+
+    //100
+    else if (time.match(/[0-9]{3}/) !== null) {
+        hour = parseInt(time.slice(0,1));
+        minute = parseInt(time.slice(1,3));
+    }
+
+    //12
+    else if(time.match(/\b[0-9]{1,2}\b/) !== null) {
+        hour = parseInt(time);
+        minute = 0;
+    }
+
+    if (typeof(hour) !== 'number' || typeof(minute) !== 'number')
+        return false;
+
+    console.log('hour: ' + hour);
+    console.log('minute: ' + minute);
+
+    //fail if out of bounds
+    if (hour > 23 || hour < 0 || minute > 59 || minute < 0)
+        return false;
+
+    lunchtime = new Date(timeobj.setHours(hour, minute));
+
+    //add 12 hours until the desired time is later than the current time
+    while (lunchtime < new Date()) {
+        lunchtime = new Date(lunchtime.getTime() + 12 * 60 * 60 * 1000);
+    }
+
+    if (lunchtime instanceof Date) {
+        return lunchtime;
+    }
+
+    else
+        return false;
+
 }
---
+++ commands/luncher.js
@@ -0,0 +1,79 @@
+
+var slack = require('../lib/slack').rtm;
+
+var schedule = {};
+
+module.exports = {
+    schedule: schedule,
+    addLunch: addLunch,
+    clearLunch: clearLunch,
+    checkDupe: checkDupe,
+    listLunch: listLunch
+};
+
+function addLunch(user, time, channel) {
+
+    if (!(this.schedule[user] instanceof Object)) {
+        this.schedule[user] = {
+            name: user,
+            time: time,
+            channel: channel,
+            notified: 0
+        };
+        console.log('time: ' + time);
+        return true;
+    }
+    else
+        return false;
+}
+
+function clearLunch(user) {
+
+    if (this.schedule[user] instanceof Object) {
+        delete this.schedule[user];
+        return true;
+    }
+    else
+        return false;
+
+}
+
+//TODO: finish this
+//checks to see if a lunch slot is already taken
+function checkDupe(time) {
+
+    for (var user in this.schedule) {
+        if ((this.schedule[user].time.getHours() === time.getHours()) && (this.schedule[user].time.getMinutes() === time.getMinutes())) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function listLunch() {
+    var lunch_list = '*Lunch times:* ';
+
+    var that = this;
+
+    if (Object.keys(this.schedule).length !== 0)
+        Object.keys(this.schedule).forEach(function (user) {
+
+            hour = that.schedule[user].time.getHours();
+            minute = that.schedule[user].time.getMinutes();
+
+            if (minute.toString().length < 2)
+                minute = '0' + minute;
+
+            lunch_list = lunch_list + user + ' (' + hour + ':' + minute + '), ';
+
+        });
+
+    else {
+        return false;
+    }
+
+    lunch_list = lunch_list.replace(/, $/, '');
+
+    return lunch_list;
+}
