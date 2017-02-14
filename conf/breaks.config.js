1f555294edf9b9f8ba573246f20f8e0c99904ed6
Alexander Houston <ahouston@liquidweb.com>
2017-02-14 07:54:44 -0500

    lowered wrapup check time a little to compensate for livechat delays

--- conf/breaks.config.js
+++ conf/breaks.config.js
@@ -6,5 +6,5 @@ module.exports = {
     /* how long to wait between reminders to log back in, in seconds */
     remindTime: 120,
     /* grace period after a chat ends, in seconds */
-    wrapupTime: 30
+    wrapupTime: 45
 };
