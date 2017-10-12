33ad42f94a6dcf81684562f6dcca66df45e30ba8
Alexander Houston <ahouston@liquidweb.com>
2017-10-12 10:02:46 -0400

    fixed bug with topic cleaner

--- commands/ping/index.js
+++ commands/ping/index.js
@@ -1,6 +1,7 @@
 var slack = require('../../lib/slack').rtm;
 var breaks = require('../breaks');
 var topic = require('../topic');
+let globals = require('../../conf/config.globals')
 
 module.exports = {
     expr: /(!ping)|(breakbot:? ping)/i,
