484b262c44d6d0cb0c6a2ec6ab06212cba0122d8
Alexander Houston <ahouston@liquidweb.com>
2017-11-20 17:21:32 -0500

    added cooldown to !remaining command output

--- commands/remaining/index.js
+++ commands/remaining/index.js
@@ -14,12 +14,22 @@ function remaining(data) {
     if (!globals.channels.hasOwnProperty(data.name))
         return false;
 
-    let breaksLeft = conf_breaks.maxDailyBreaks;
+    let remMsg = 'm until cooldown expires)',
+        breaksLeft = conf_breaks.maxDailyBreaks,
+        cooldownRemaining;
+
+    if (!!globals.channels[data.name].breaks.cooldown[data.username]) {
+        cooldownTime = new Date().getTime() - globals.channels[data.name].breaks.cooldown[data.username].getTime();
+        cooldownRemaining = Math.ceil(Math.abs(cooldownTime / 60 / 1000));
+        remMsg = '\n(' + cooldownRemaining.toString() + remMsg
+    }
+    else
+        remMsg = '';
 
     if (globals.channels[data.name].breaks.count.hasOwnProperty(data.username))
         breaksLeft -= globals.channels[data.name].breaks.count[data.username];
 
-    slack.sendMessage('breaks remaining: ' + breaksLeft, data.channel);
+    slack.sendMessage('breaks remaining: ' + breaksLeft + remMsg, data.channel);
 
     /* logging */
     let logdata = {
