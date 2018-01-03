c23498d8b37f38a7415f0f76478933d36874d27b
Alexander Houston <ahouston@liquidweb.com>
2018-01-03 17:55:56 -0500

    updated for consistency

--- commands/add/index.js
+++ commands/add/index.js
@@ -1,5 +1,8 @@
+const _ = require('lodash');
+
 let slack = require('../../lib/slack').rtm;
 let topic = require('../topic');
+let H
 
 let globals = require('../../conf/config.globals');
 
@@ -13,21 +16,35 @@ module.exports = {
  */
 function add(data) {
 
+    //assume user wants to add their own name if none is provided
+    let arg = data.text? data.text.split(' '): [data.username];
+
+    //console.log(`arg: ${arg}`);
+    // let chanObj = globals.channels[data.name],
+    //     arg = data.text.split(' '),
+    //     subject = arg
+    //                 ?slack.getUser(arg) //arg exists
+    //                     ?slack.getUser(arg).name //arg has slack acct, set subject to slack acct's name
+    //                     :data.username //arg does not have a slack acct, set subject to sender's username
+    //                 :data.username; //arg does not exist, set subject to sender's username
+
+    // console.log(`${subject}? ${chanObj.isInTopic(subject)}`);
+
     /* try to parse the arg if given. if it can't, defaults to the user that sent the message*/
-    let arg = data.text.split(' ')
-        .map(function (el) {
-            el = topic.removeSpecial(el);
-            if (el.match(/^me$/i))
-                return slack.getUser(data.user).name;
-            else
-                return slack.getUser(el).name;
-        });
 
-    if (arg instanceof Array)
-        if (arg.length === 0)
-            arg = [slack.getUser(data.user).name];
+    // if (!chanObj.isInTopic(subject)) {
+    //     console.error('already in topic');
+    //     return false;
+    // }
 
-    topic.setTopic(data, globals.channels[data.name].topic + ' ' + arg.join(' '));
+    arg = arg.map(el => {
+        el = topic.removeSpecial(el);
+        return el.match(/^me$/i)? data.username: slack.getUser(el).name;
+    });
 
-    console.log(new Date().toLocaleString() + ' ADD: updated topic in ' + data.name + ': ' + globals.channels[data.name].topic)
+    arg = _.uniqBy(arg, _.toLower);
+
+    topic.setTopic(data, `${globals.channels[data.name].topic} ${arg.join(' ')}`);
+
+    console.log(new Date().toLocaleString(), `ADD: updated topic in ${data.name}:`, globals.channels[data.name].topic)
 }
