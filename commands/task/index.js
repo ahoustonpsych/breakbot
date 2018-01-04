cac53bd74160ce4d7759366a40dd91d7a1c714d4
Alexander Houston <ahouston@liquidweb.com>
2018-01-04 17:14:41 -0500

    nixed the multi-user !task functionality. rarely comes up, no real need for it

--- commands/task/index.js
+++ commands/task/index.js
@@ -98,46 +98,6 @@ function task(data) {
             console.error(new Date().toLocaleString() + ' ERROR PARSING TASK TIME', err);
         });
 
-    /* not used, uncomment if it becomes necessary to set multiple people on task at once */
-    /* split up the list of users, discarding 'invalid' ones except for 'me' */
-    // users = data.text.split(' ')
-    //     .slice(off)
-    //     .filter(function (el) {
-    //
-    //         /* match 'me' for later use */
-    //         if (el.match(/^me$/i) !== null)
-    //             return true;
-    //
-    //         /* match usernames if given, and discard the rest */
-    //         else
-    //             return (slack.dataStore.getUserByName(el) instanceof Object);
-    //
-    //     })
-    //     .map(function (el) {
-    //
-    //         /* replace 'me' with your username if given */
-    //         if (el.match(/^me$/i) !== null)
-    //             return data.username;
-    //
-    //         /* otherwise, don't do anything */
-    //         return el;
-    //
-    //     });
-
-    /* if we have an invalid array somehow, just default to the user that sent the message*/
-    // if (users instanceof Array) {
-    //     if (users.length === 0) {
-    //         users = [user];
-    //     }
-    // }
-
-    // else
-         //users = [user];
-
-    /* log task users */
-    // if (users instanceof Array)
-    //     putOnTask(data, users);
-
 }
 
 function setTask(user, time, reason, chanObj) {
